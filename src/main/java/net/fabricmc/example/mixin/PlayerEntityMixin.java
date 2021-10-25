db15146ac061074e7b7173d5f7fa9ad16a4d9a4e
Rudy Soliz <rudy@rudysoliz.com>
2021-10-25 06:54:55 -0500

    broken ):

---
+++ src/main/java/net/fabricmc/example/mixin/PlayerEntityMixin.java
@@ -0,0 +1,19 @@
+package net.fabricmc.example.mixin;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Serializable;
+
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin {
+
+}
